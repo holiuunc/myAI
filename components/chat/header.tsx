fd31f4a9c4532d2f854f429e20156688503fe0e3
Hong Liu <hong.liu@parkingpinllc.com>
2025-03-11 22:48:20 -0400

    Update to the icon

--- components/chat/header.tsx
+++ components/chat/header.tsx
@@ -7,7 +7,7 @@ import { AI_NAME } from "@/configuration/identity";
 export const AILogo = () => (
   <div className="w-12 h-12 relative">
     <Image src="/ai-logo.png" alt={AI_NAME} width={48} height={48} />
-    <div className="w-2 h-2 rounded-full bg-green-500 absolute -bottom-0.5 -right-0.5"></div>
+    <div className="w-2 h-2 rounded-full bg-green-500 absolute -bottom-0.5 -right-0.5" />
   </div>
 );
 
@@ -19,7 +19,7 @@ export default function ChatHeader({
   return (
     <div className="z-10 flex justify-center items-center fixed top-0 w-full p-5 bg-white shadow-[0_10px_15px_-3px_rgba(255,255,255,1)]">
       <div className="flex w-full">
-        <div className="flex-0 w-[100px]"></div>
+        <div className="flex-0 w-[100px]" />
         <div className="flex-1 flex justify-center items-center gap-2">
           <AILogo />
           <p>{CHAT_HEADER}</p>
